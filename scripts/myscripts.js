4e4e4ab9e90bc07ff8a4df5ff8a2ac0440405066
avanfleet <58604129+avanfleet@users.noreply.github.com>
2019-12-15 21:10:11 -0600

    assignment2
    assignment2

---
+++ scripts/myscripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){   
+});
+
+//i think you need to create OG variables too?
+var female = prompt("Please enter a female’s first name (1 of 3)");
+
+var uncle = prompt("Please enter a male’s first name (1 of 3)");
+
+var aunt = prompt("Please enter a female’s first name (1 of 3)");
+
+var geofig = prompt("Please enter a female’s first name (1 of 3)");
+
+var state = prompt("Please enter a state.");
+
+var occupation = prompt("Please enter an occupation.");
+
+var color = prompt("Please enter a color");
+
+
+//an array
+var nouns = ["noun1", "noun2", "noun3", "noun4", "noun5"];
+var nouns = prompt("Please enter a noun");
